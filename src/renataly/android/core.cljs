77e0643470f2bbb1760a448478dcd0fb8057e9cb
Matthew Orlando <maorlando@gmail.com>
2017-04-06 16:31:50 -0700

    Props

--- src/renataly/android/core.cljs
+++ src/renataly/android/core.cljs
@@ -17,16 +17,23 @@
 (defn alert [title]
       (.alert (.-Alert ReactNative) title))
 
-(defn app-root []
-  (let [greeting (subscribe [:get-greeting])]
+(defn greeting [props]
+  (let [greeting-text (subscribe [:get-greeting])]
     (fn []
-      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
-       [image {:source logo-img
-               :style  {:width 80 :height 80 :margin-bottom 30}}]
-       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                             :on-press #(alert "HELLO!")}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+      [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}}
+       (str @greeting-text (:name props))])))
+
+(defn app-root []
+  [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+   [greeting {:name "Cog"}]
+   [greeting {:name "Juliella"}]
+   [greeting {:name "Jeaye"}]
+   [greeting {:name "Penny"}]
+   [image {:source logo-img
+           :style  {:width 80 :height 80 :margin-bottom 30}}]
+   [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+                         :on-press #(alert "HELLO!")}
+    [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])
 
 (defn init []
       (dispatch-sync [:initialize-db])
