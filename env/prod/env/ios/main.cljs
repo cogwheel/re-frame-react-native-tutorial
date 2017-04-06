dc839c52f6ab83fede00692b20111234da40100a
Matthew Orlando <maorlando@gmail.com>
2017-04-06 15:48:08 -0700

    Initial commit

---
+++ env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [renataly.ios.core :as core]))
+
+ (core/init)
+
+
---
+++ test/renataly/core_test.clj
@@ -0,0 +1,7 @@
+(ns renataly.core-test
+  (:require [clojure.test :refer :all]
+            [renataly.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
